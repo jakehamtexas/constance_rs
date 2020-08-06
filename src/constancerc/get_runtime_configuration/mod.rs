47c6dfb344401e611ffff48b97efa4da49f68f4e
Jake Hamilton <jakehamtexas@gmail.com>
2020-08-05 22:00:40 -0500

    Moves manager into mod

--- src/constancerc/get_runtime_configuration/mod.rs
+++ src/constancerc/get_runtime_configuration/mod.rs
@@ -1,3 +1,21 @@
+use super::dto::ConstanceRc;
+use file_system::{FileSystem, RcFileExtension};
+use rc_parser::RcParser;
+
 pub mod file_system;
-pub mod manager;
 pub mod rc_parser;
+
+pub fn get_runtime_configuration(
+    path: String,
+    file_system: impl FileSystem,
+    rc_parser: impl RcParser,
+) -> ConstanceRc {
+    let buf = file_system
+        .get_file(path)
+        .expect(&format!("No configuration file found at path: {}", path));
+    match file_system.get_extension(path) {
+        RcFileExtension::Json => rc_parser.from_json(buf),
+        RcFileExtension::Yaml => rc_parser.from_yaml(buf),
+        _ => panic!("Unrecognized file type!"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use constancerc::get_runtime_configuration::file_system::FileSystem;
+
 mod constancerc;
 mod reader;
 mod table_to_constants;
 mod write_files_for_targets;
 fn main() {
-    let rc = constancerc::get_runtime_configuration::manager::get_runtime_configuration();
+    let rc = constancerc::get_runtime_configuration::get_runtime_configuration();
 
     let table_options = &rc.table_options;
     let output_options = &rc.output_options;
