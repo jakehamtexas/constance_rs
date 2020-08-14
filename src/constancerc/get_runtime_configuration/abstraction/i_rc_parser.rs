8d9e846c81793a7a489fe215fad6ff0c00c4da6a
Jake Hamilton <jakehamtexas@gmail.com>
2020-08-14 09:51:24 -0500

    Removes lifetime params from i_rc_parser and impl since they're elided anyway.

--- src/constancerc/get_runtime_configuration/abstraction/i_rc_parser.rs
+++ src/constancerc/get_runtime_configuration/abstraction/i_rc_parser.rs
@@ -1,5 +1,5 @@
 use super::super::super::dto::ConstanceRc;
 pub trait IRcParser {
-    fn from_yaml<'a>(&self, buf: &str) -> Option<ConstanceRc>;
-    fn from_json<'a>(&self, buf: &str) -> Option<ConstanceRc>;
+    fn from_yaml(&self, buf: &str) -> Option<ConstanceRc>;
+    fn from_json(&self, buf: &str) -> Option<ConstanceRc>;
 }
--- src/constancerc/get_runtime_configuration/concrete/rc_parser.rs
+++ src/constancerc/get_runtime_configuration/concrete/rc_parser.rs
@@ -5,11 +5,11 @@ use serde_yaml::Result as YamlResult;
 pub struct RcParser {}
 
 impl IRcParser for RcParser {
-    fn from_yaml<'a>(&self, buf: &str) -> Option<ConstanceRc> {
+    fn from_yaml(&self, buf: &str) -> Option<ConstanceRc> {
         let rc: YamlResult<ConstanceRc> = serde_yaml::from_str(buf);
         rc.ok()
     }
-    fn from_json<'a>(&self, buf: &str) -> Option<ConstanceRc> {
+    fn from_json(&self, buf: &str) -> Option<ConstanceRc> {
         let rc: JsonResult<ConstanceRc> = serde_json::from_str(buf);
         rc.ok()
     }
