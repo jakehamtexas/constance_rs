7f91ecad08b8d5682a936bda0bfd48615cde6151
Jake Hamilton <jakehamtexas@gmail.com>
2020-08-15 20:20:33 -0500

    Reorganizes redeclarations in lib and usage in main

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,20 @@ mod reader;
 mod table_to_constants;
 mod write_files_for_targets;
 
-pub use constancerc::get_runtime_configuration::get_runtime_configuration;
-pub use reader::manager::get_database;
-pub use table_to_constants::manager::get_table_constants;
-pub use write_files_for_targets::manager::write_files_for_targets;
+pub mod functions {
+    pub use super::constancerc::get_runtime_configuration::get_runtime_configuration;
+    pub use super::reader::manager::get_database;
+    pub use super::table_to_constants::manager::get_table_constants;
+    pub use super::write_files_for_targets::manager::write_files_for_targets;
+}
 
-pub use constancerc::get_runtime_configuration::concrete::{
-    cli_args::CliArgs, file_system::FileSystem, rc_parser::RcParser,
-};
+pub mod types {
+    pub use super::constancerc::dto::{output_options::OutputOptions, ConstanceRc};
+    pub use super::constancerc::get_runtime_configuration::concrete::{
+        cli_args::CliArgs, file_system::FileSystem, rc_parser::RcParser,
+    };
+
+    pub use super::constancerc::get_runtime_configuration::abstraction::{
+        i_cli_args::ICliArgs, i_file_system::IFileSystem, i_rc_parser::IRcParser,
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use constance::{
-    get_database, get_runtime_configuration, get_table_constants, write_files_for_targets, CliArgs,
-    FileSystem, RcParser,
+    functions::{
+        get_database, get_runtime_configuration, get_table_constants, write_files_for_targets,
+    },
+    types::{CliArgs, FileSystem, OutputOptions, RcParser},
 };
 
 fn main() {
@@ -10,11 +12,13 @@ fn main() {
     let rc = get_runtime_configuration(cli_args, file_system, rc_parser);
 
     let table_options = &rc.table_options;
-    let output_options = &rc.output_options;
 
     let db = get_database(&rc);
 
     let table_constants = get_table_constants(db, table_options);
 
+    let output_options = &rc
+        .output_options
+        .unwrap_or_else(|| OutputOptions::default());
     write_files_for_targets(&table_constants, &output_options);
 }
