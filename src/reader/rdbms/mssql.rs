455428dc9f91ad6d8dc20dca658d4f8546bc64ed
Jake Hamilton <jakehamtexas@gmail.com>
2020-09-05 21:24:10 -0500

    Implements get_records_as_simple_key_value_pairs

--- src/reader/rdbms/mssql.rs
+++ src/reader/rdbms/mssql.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 use async_trait::async_trait;
 use std::collections::HashMap;
-use tiberius::{error::Error, AuthMethod, Client, Config};
+use tiberius::{AuthMethod, Client, Config};
 use tokio::net::TcpStream;
 use tokio_util::compat::{Compat, Tokio02AsyncWriteCompatExt};
 
@@ -31,9 +31,8 @@ impl Mssql {
         Self { connection }
     }
 
-    pub async fn get_client(&self) -> Result<Client<Compat<TcpStream>>, Error> {
-        let connection = &self.connection;
-        let config = match connection {
+    fn get_config(&self, database_name: &str) -> Config {
+        let mut config = match &self.connection {
             Connection {
                 connection_string: Some(connection_string),
                 connection_options: Some(options),
@@ -46,11 +45,16 @@ impl Mssql {
             } => from_options(options),
             _ => panic!("No connection configuration is available!"),
         };
+        config.database(database_name);
+        config
+    }
 
-        let tcp = TcpStream::connect(config.get_addr()).await?;
-        tcp.set_nodelay(true)?;
+    pub async fn get_client(&self, table_option: &TableOption) -> Client<Compat<TcpStream>> {
+        let config = self.get_config(&table_option.identifier.database_name);
+        let tcp = TcpStream::connect(config.get_addr()).await.unwrap();
+        tcp.set_nodelay(true).unwrap();
 
-        Client::connect(config, tcp.compat_write()).await
+        Client::connect(config, tcp.compat_write()).await.unwrap()
     }
 }
 
@@ -60,11 +64,29 @@ impl ReadDb for Mssql {
         &self,
         table_option: &TableOption,
     ) -> HashMap<String, String> {
-        let client = self.get_client().await.unwrap();
+        let mut client = self.get_client(table_option).await;
         let key_column_name = &table_option.key_column_name;
         let value_column_name = table_option.value_column_names.first().unwrap();
 
-        todo!()
+        let query_result = client
+            .query(
+                format!("SELECT {}, {}", key_column_name, value_column_name),
+                &[],
+            )
+            .await;
+        let rows = match query_result {
+            Ok(query) => query.into_first_result().await.unwrap(),
+            _ => panic!("No results for given query parameters: [ key_column_name: {}, value_column_name: {} ]", key_column_name, value_column_name)
+        };
+
+        rows.into_iter().fold(HashMap::new(), |mut map, row| {
+            let key_column = row.get::<i32, &str>(key_column_name);
+            let value_column = row.get::<&str, &str>(value_column_name);
+            if let (Some(key), Some(value)) = (key_column, value_column) {
+                map.insert(key.to_string().to_owned(), value.to_owned());
+            }
+            map
+        })
     }
 
     async fn get_records_with_meta_description_column(
