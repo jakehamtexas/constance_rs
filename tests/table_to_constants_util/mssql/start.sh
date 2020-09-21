87f98a97d9d8669ca6aaf756b0f2692442622ff4
Jake Hamilton <jakehamtexas@gmail.com>
2020-09-20 22:15:35 -0500

    Add sql generation and test teardown to mssql integration test start script

--- tests/table_to_constants_util/mssql/start.sh
+++ tests/table_to_constants_util/mssql/start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Setup
+(cd pretest && cargo run --release ../values)
 pw=MyAwS0M3P4SsW0Rd!
 docker run -d -p 1433:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=$pw --rm --name mssql mcr.microsoft.com/mssql/server:2017-CU12-ubuntu
 docker ps -a
 docker cp init.sql mssql:/init.sql
 docker exec mssql sh -c "/opt/mssql-tools/bin/sqlcmd -H localhost -U sa -P $pw -i ./init.sql"
+
+# Run the tests!
+
+# Teardown
+rm init.sql
+docker kill mssql
+printf 'y\n' | docker system prune -a
