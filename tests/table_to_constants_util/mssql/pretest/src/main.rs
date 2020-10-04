a246eb755ecca35761b05fde4bfa1e0e50afd339
Jake Hamilton <jakehamtexas@gmail.com>
2020-10-03 22:08:57 -0500

    Define string_enum_create_table

--- tests/table_to_constants_util/mssql/pretest/src/main.rs
+++ tests/table_to_constants_util/mssql/pretest/src/main.rs
@@ -19,7 +19,7 @@ fn without_first_char(s: &str) -> String {
         .to_owned()
 }
 
-fn get_create_table_statement(table_name: &str, columns: &[Column]) -> Result<String, sql::Error> {
+fn get_create_table_statement(table_name: &str, columns: &[&Column]) -> Result<String, sql::Error> {
     columns
         .iter()
         .fold(create_table(table_name), |create_table, column| {
@@ -34,7 +34,7 @@ fn get_create_table_statement(table_name: &str, columns: &[Column]) -> Result<St
 
 fn get_raw_insert_statement(
     table_name: &str,
-    columns: &[Column],
+    columns: &[&Column],
     batch_size: usize,
 ) -> Result<String, sql::Error> {
     insert_into(table_name)
@@ -84,19 +84,33 @@ fn main() {
 
     let id_column = Column::Pkey;
     let name_column = Column::Text("name");
-    let table_name = "simple_enum";
+    let simple_enum_table_name = "simple_enum";
 
-    let columns = vec![id_column, name_column];
-    let simple_enum_create_table = get_create_table_statement(table_name, &columns).unwrap();
-    let simple_enum_raw_insert =
-        get_raw_insert_statement(table_name, &columns, simple_enum_json.len()).unwrap();
+    let simple_enum_columns = vec![&id_column, &name_column];
+    let simple_enum_create_table =
+        get_create_table_statement(simple_enum_table_name, &simple_enum_columns).unwrap();
+
+    let string_id_column = Column::Text("string_id");
+    let string_enum_table_name = "string_enum";
+
+    let string_enum_columns = vec![&id_column, &name_column, &string_id_column];
+
+    let string_enum_create_table =
+        get_create_table_statement(string_enum_table_name, &string_enum_columns).unwrap();
+
+    let simple_enum_raw_insert = get_raw_insert_statement(
+        simple_enum_table_name,
+        &simple_enum_columns,
+        simple_enum_json.len(),
+    )
+    .unwrap();
 
     let simple_enum_insert = simple_enum_json.iter().enumerate().fold(
         simple_enum_raw_insert,
         |simple_enum_insert, (index, name)| {
             let first_two_value_indices = simple_enum_insert
                 .match_indices("?")
-                .take(columns.len())
+                .take(simple_enum_columns.len())
                 .map(|(index, _)| index)
                 .collect::<Vec<_>>();
             let (first_index, second_index) = (
@@ -112,6 +126,10 @@ fn main() {
             [before.to_owned(), between_with_value, after_with_value].join("")
         },
     );
-    let sql = get_sql(&[&simple_enum_create_table, &simple_enum_insert]);
+    let sql = get_sql(&[
+        &simple_enum_create_table,
+        &simple_enum_insert,
+        &string_enum_create_table,
+    ]);
     std::fs::write("../init.sql", sql).expect("Unable to write to file.");
 }
