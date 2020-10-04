ee92fff080c10638f5b0a063a0306b7f127dfbd8
Jake Hamilton <jakehamtexas@gmail.com>
2020-10-04 18:48:32 -0500

    Simplify insert statement generation

--- tests/table_to_constants_util/mssql/pretest/src/string_enum.rs
+++ tests/table_to_constants_util/mssql/pretest/src/string_enum.rs
@@ -1,9 +1,17 @@
 use crate::{
     column::{Column, ID_COLUMN, NAME_COLUMN},
+    insert_utils::to_substituted,
     sql_util::get_create_table_statement,
     sql_util::get_raw_insert_statement,
-    sql_util::without_first_char,
 };
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StringEnum {
+    name: String,
+    string_id: String,
+}
 
 const TABLE_NAME: &'static str = "string_enum";
 static STRING_ID_COLUMN: Column = Column::Text("string_id");
@@ -13,7 +21,13 @@ pub fn create_table_statement() -> Result<String, sql::Error> {
     get_create_table_statement(TABLE_NAME, &COLUMNS)
 }
 
-pub fn insert_statement(json: &Vec<&str>) -> Result<String, sql::Error> {
-    let _raw = get_raw_insert_statement(TABLE_NAME, &COLUMNS, json.len())?;
-    todo!();
+pub fn insert_statement<'a>(json: &'a Vec<StringEnum>) -> Result<String, sql::Error> {
+    let raw = get_raw_insert_statement(TABLE_NAME, &COLUMNS, json.len())?;
+    Ok(json
+        .iter()
+        .enumerate()
+        .map(|(index, StringEnum { name, string_id })| {
+            vec![index.to_string(), name.to_owned(), string_id.to_owned()]
+        })
+        .fold(raw, |statement, args| to_substituted(&statement, &args)))
 }
